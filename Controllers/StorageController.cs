008c471ae470bbcff6a3727fe73d4bc173ac2a97
Alesya Deschenya <69769972+dealeska@users.noreply.github.com>
2021-05-19 01:10:18 +0300

    Add files via upload

---
+++ Controllers/StorageController.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.StaticFiles;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace FileStorage.Controllers
+{
+    [Route("FileStorage")]
+    [ApiController]
+    public class StorageController : ControllerBase
+    {
+        private readonly ILogger<StorageController> _logger;
+        private readonly string _path = @"C:\Storage";
+
+        private const string CopyHeader = "X-Copy-From";        
+         
+        public StorageController(ILogger<StorageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{*path}")]
+        public ActionResult GetFile(string path)
+        {
+            string fullPath = FileStorageService.GetFullPath(path);
+            
+            if (Directory.Exists(fullPath))
+            {
+                try
+                {
+                    List<string> content = FileStorageService.GetDirectoryInfo(fullPath);                  
+                    return new JsonResult(content);
+                }
+                catch
+                {
+                    return StatusCode(500);
+                }
+
+            }
+            else if (System.IO.File.Exists(fullPath))
+            {
+                try
+                {                    
+                    FileStream fileStream = new FileStream(fullPath, FileMode.Open);
+                    return File(fileStream, FileStorageService.GetMimeType(fullPath), Path.GetFileName(fullPath));
+                }
+                catch
+                {
+                    return StatusCode(500);
+                }
+
+            }
+
+            return NotFound();
+        }
+
+
+        [HttpDelete("{*fileName}")]
+        public ActionResult DeleteFile(string fileName)
+        {
+            string fullPath;
+
+            if (fileName == null)
+            {                 
+                return BadRequest();
+            }
+            else
+            {
+                fullPath = FileStorageService.GetFullPath(fileName);
+            }
+
+            try
+            {
+                if (Directory.Exists(fullPath))
+                {
+                    Directory.Delete(fullPath, true);
+                    return Ok();
+                }
+                else if (System.IO.File.Exists(fullPath))
+                {
+                    System.IO.File.Delete(fullPath);
+                    return Ok();
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+
+            return NotFound();
+        }
+
+
+        [HttpHead("{*fileName}")]
+        public ActionResult GetFileHeader(string fileName)
+        {
+            string fullPath = FileStorageService.GetFullPath(fileName);
+
+            try
+            {                
+                FileInfo fileInfo = new FileInfo(fullPath);
+                if (fileInfo.Exists)
+                {
+                    Response.Headers.Add("Name", fileInfo.Name);
+                    Response.Headers.Add("Path", fileInfo.DirectoryName);
+                    Response.Headers.Add("Content-Type", FileStorageService.GetMimeType(fullPath));
+                    Response.Headers.Add("Size", fileInfo.Length.ToString());
+                    Response.Headers.Add("Creation-date", fileInfo.CreationTime.ToString());
+                    Response.Headers.Add("Changed-date", fileInfo.LastWriteTime.ToString());
+                    return Ok();
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+
+            return NotFound();
+        }
+        
+
+        [HttpPut("{*path}")]
+        public ActionResult PutFile(string path)
+        {
+            string fullPath = FileStorageService.GetFullPath(path);
+
+            IFormFileCollection formFiles;
+            try
+            {
+                formFiles = Request.Form.Files;
+            }
+            catch
+            {
+                formFiles = null;
+            }
+
+            try
+            {                
+                if (Request.Headers.ContainsKey(CopyHeader))
+                {                    
+                    int code = FileStorageService.CopyFile(Request.Headers[CopyHeader], fullPath);
+
+                    if (code == FileStorageService.ok)
+                    {
+                        return Ok();
+                    }
+                    else if (code == FileStorageService.bad)
+                    {
+                        return BadRequest();
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }                
+                else if (Directory.Exists(fullPath))
+                {
+                    var file = formFiles[0];
+                    using (FileStream fs = new FileStream(Path.Combine(fullPath, file.FileName), FileMode.Create))
+                    {
+                        file.CopyTo(fs);
+                    }
+
+                    return Ok();
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ FileStorageService.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FileStorage
+{
+    public static class FileStorageService
+    {
+        public const int ok = 1;
+        public const int bad = 2;
+        public const int notFound = 4;
+
+        private const string _path = @"C:\Storage";
+         
+        public static string GetFullPath(string filePath)
+        {
+            if (filePath == null)
+            {
+                return _path;
+            }
+            else
+            {
+                return Path.Combine(_path, filePath);
+            }
+        }
+
+        public static List<string> GetDirectoryInfo(string path)
+        {
+            List<string> information = new List<string>();
+            string[] directories = Directory.GetDirectories(path);
+
+            foreach (var directory in directories)
+            {
+                information.Add($"Folder: {new DirectoryInfo(directory).Name}");
+            }
+
+            string[] files = Directory.GetFiles(path);
+
+            foreach (var file in files)
+            {
+                information.Add($"File: {new FileInfo(file).Name}");
+            }
+
+            return information;
+        }
+
+        public static string GetMimeType(string path)
+        {
+            string mimeType = "application/unknown";
+            string ext = Path.GetExtension(path).ToLower();
+            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
+            if (regKey != null && regKey.GetValue("Content Type") != null)
+                mimeType = regKey.GetValue("Content Type").ToString();
+
+            return mimeType;
+        }
+
+        public static void AddFileInfo(string path)
+        {
+
+        }
+
+        public static List<string> ProcessDirectory(string directory)
+        {
+            List<string> result = new List<string>();
+
+            string[] directoryEntities = Directory.GetDirectories(directory);
+            foreach(string directoryName in directoryEntities)
+            {
+                result.Add($"Directory: {new DirectoryInfo(directoryName).Name}");
+            }
+
+            string[] fileEntries = Directory.GetFiles(directory);
+            foreach (string fileName in fileEntries)
+            {
+                result.Add($"File: {Path.GetFileName(fileName)}");
+            }
+
+            return result;
+        }       
+
+        public static Dictionary<string, string> FileHeader(string filePath)
+        {
+            Dictionary<string, string> header = new Dictionary<string, string>();
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            header.Add("Name: ", fileInfo.Name);
+            header.Add("Size: ", fileInfo.Length.ToString());
+            header.Add("Extension: ", fileInfo.Extension);
+            header.Add("Creation time: ", fileInfo.CreationTime.ToString());
+            header.Add("Last write time:", fileInfo.LastWriteTime.ToString());
+            header.Add("Last access time: ", fileInfo.LastAccessTime.ToString());
+
+            return header;
+        }
+
+        public static int CopyFile(string srcPath, string destPath)
+        {
+            srcPath = Path.Combine(_path, srcPath);
+
+            if (!File.Exists(srcPath))
+            {
+                return notFound;
+            }
+
+            if (!Directory.Exists(Path.GetDirectoryName(destPath)))
+            {
+                return notFound;
+            }
+
+            if (!Path.GetFileName(srcPath).Contains(".") || !Path.GetFileName(destPath).Contains("."))
+            {
+                return bad;
+            }
+
+            if (srcPath != destPath)
+            {
+                using FileStream sourceFile = new FileStream(srcPath, FileMode.Open);
+                using FileStream destFile = new FileStream(destPath, FileMode.OpenOrCreate);
+                sourceFile.CopyTo(destFile);
+            }
+            
+            return ok;
+        }
+
+    }
+}
